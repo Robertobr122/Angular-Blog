2da838d41a812fe54cbf9d62105ab50f553f737b
robertobr122 <rbbragaengenharia@gmail.com>
2024-11-14 11:21:11 -0300

    components creates with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import{MenuTitleComponent} from './components/menu-title/menu-title.component';
+import{MenuBarComponent} from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTitleComponent,
+    MenuBarComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    RouterModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
